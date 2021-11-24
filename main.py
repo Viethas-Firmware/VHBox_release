789626e7e821c4e54a441d1444f43f4e24231c1f
redhat2410 <ducvuminhspk@gmail.com>
2021-11-24 13:40:38 +0700

    upload by ducvu

---
+++ main.py
@@ -0,0 +1,17 @@
+from config.define import *
+
+if USE_GUI == True:
+    # import library use gui
+    pass
+
+import os
+import sys
+import time
+from process import PROCESS
+from modules.upgrade import UPGRADE
+
+
+if __name__ == "__main__":
+    # PROCESS.main()
+    # up = UPGRADE.backup()
+    down = UPGRADE.download()
