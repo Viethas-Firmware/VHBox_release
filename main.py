013cc6f341ef39717adde01ca368bd9a6263d244
redhat2410 <ducvuminhspk@gmail.com>
2021-11-26 10:45:17 +0700

    upload by ducvu

--- main.py
+++ main.py
@@ -7,12 +7,34 @@ if USE_GUI == True:
 import os
 import sys
 import time
+import json
 from process import PROCESS
 from modules.upgrade import UPGRADE
 
 
 if __name__ == "__main__":
-    # PROCESS.main()
+    PROCESS.main()
     # up = UPGRADE.backup()
     # down = UPGRADE.download()
-    install = UPGRADE.checksum()
+    # install = UPGRADE.install()
+    # if os.path.isfile(PATH_CONFIG_VERSION):
+    #     f_version = open(PATH_CONFIG_VERSION, 'r')
+    #     version = json.load(f_version)
+    #     f_version.close()
+    #     print(version['version'])
+    # while True:
+    #     cmd = input("enter command: ")
+
+    #     if cmd == "download":
+    #         down = UPGRADE.download()
+    #     elif cmd == "install":
+    #         install = UPGRADE.install()
+    #         print(install)
+    #     elif cmd == "check":
+    #         f_version = open(PATH_CONFIG_VERSION, 'r')
+    #         version = json.load(f_version)
+    #         f_version.close()
+
+    #         print(version['version'])
+    #     else:
+    #         break
