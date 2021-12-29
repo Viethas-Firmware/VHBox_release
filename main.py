d158145e62254c380e9583d05475451be18ba9d3
redhat2410 <ducvuminhspk@gmail.com>
2021-12-29 18:30:22 +0700

    upload by ducvu:

--- main.py
+++ main.py
@@ -12,29 +12,6 @@ from process import PROCESS
 from modules.upgrade import UPGRADE
 
 
+
 if __name__ == "__main__":
     PROCESS.main()
-    # up = UPGRADE.backup()
-    # down = UPGRADE.checksum()
-    # install = UPGRADE.install()
-    # if os.path.isfile(PATH_CONFIG_VERSION):
-    #     f_version = open(PATH_CONFIG_VERSION, 'r')
-    #     version = json.load(f_version)
-    #     f_version.close()
-    #     print(version['version'])
-    # while True:
-    #     cmd = input("enter command: ")
-
-    #     if cmd == "download":
-    #         down = UPGRADE.download()
-    #     elif cmd == "install":
-    #         install = UPGRADE.install()
-    #         print(install)
-    #     elif cmd == "check":
-    #         f_version = open(PATH_CONFIG_VERSION, 'r')
-    #         version = json.load(f_version)
-    #         f_version.close()
-
-    #         print(version['version'])
-    #     else:
-    #         break
